513518f9280f4ad3edd0c6a46d4fff2b9ba52e4b
Jose Schafer <joseignacio.schafer@gmail.com>
2025-02-24 20:32:00 -0300

    Fix issue when refreshing page

--- src/config/protected-route.tsx
+++ src/config/protected-route.tsx
@@ -1,25 +1,33 @@
-import { Outlet, Navigate } from 'react-router';
-import { useAuth } from '@/providers/AuthContext';
+import { Outlet, Navigate } from "react-router";
+import { useAuth } from "@/providers/AuthContext";
 
 type ProtectedRouteProps = {
   allowedRoles: string[];
   redirectPath?: string;
   children?: React.ReactNode;
-}
+};
 
-function ProtectedRoute({ allowedRoles, redirectPath = '/', children }: ProtectedRouteProps) {
-  const { user } = useAuth()
+function ProtectedRoute({
+  allowedRoles,
+  redirectPath = "/",
+  children,
+}: ProtectedRouteProps) {
+  const { user } = useAuth();
+
+  if (Object.keys(user).length === 0) {
+    return;
+  }
 
   if (allowedRoles.includes("*")) {
     return children ? children : <Outlet />;
   }
 
   if (!allowedRoles.includes(user?.role.toLowerCase())) {
-    console.log("redirect")
+    console.log("redirect");
     return <Navigate to={redirectPath} replace />;
   }
 
-  console.log("allowed")
+  console.log("allowed");
   return children ? children : <Outlet />;
 }
 
