aaa78d567e72bb8e611850c8ace7c93fe472c4f9
Jose Schafer <joseignacio.schafer@gmail.com>
2025-02-24 22:01:42 -0300

    Config for PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -12,7 +39,7 @@ export default defineConfig({
   },
   server: {
     headers: {
-      "X-FRAME-OPTIONS": "DENY"
-    }
-  }
-})
+      "X-FRAME-OPTIONS": "DENY",
+    },
+  },
+});
