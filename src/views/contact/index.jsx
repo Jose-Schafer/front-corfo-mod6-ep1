dea2b6d0e7723be5f829e2903072abf13f7b705a
Jose Schafer <joseignacio.schafer@gmail.com>
2025-02-26 21:36:42 -0300

    Camera

--- src/views/contact/index.jsx
+++ src/views/contact/index.jsx
@@ -1,10 +1,12 @@
-import Form from './form'
+import Form from "./form";
+import Camara from "./camera";
 
 export default function Contact() {
   return (
     <>
       <h1>Contacto</h1>
       <Form />
+      <Camara />
     </>
-  )
+  );
 }
